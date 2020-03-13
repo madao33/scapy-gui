182eebc133cbeda3724b968219e0a5487a853fba
madao33 <32134682+Hokagesensi@users.noreply.github.com>
2020-03-13 15:41:56 +0800

    add userProto analyse

--- mainGui.py
+++ mainGui.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import ttk
 from tkinter import filedialog
 from tkinter import scrolledtext
+from tkinter import messagebox
 import dataAna
 import protoAna
 from scapy.all import *
@@ -26,120 +27,99 @@ link = Entry(root,
             font=18)
 link.grid(row=0,column=1)
 
-# 自定义配置协议
+# 本地保存的pcap文件
 userText = Label(root, 
-            text='user key(exactly 8 bytes!)',
+            text='filename',
             font=18,
             bd='2')
 userText.grid(row=0,column=2)
 
 fileString = StringVar()
-decodeKey = Entry(root,
+filePath = Entry(root,
             text=fileString,
             font=18,
-            width=40,
+            width=70,
             fg='black')
-decodeKey.grid(row=0,column=3)
+filePath.grid(row=0,column=3)
 
+def openFile(event):
+    filename = filedialog.askopenfilename()
+    print("选择文件：", filename)
+
+    filePath.delete(0, END)
+    filePath.insert(0,filename)
+
+btn_open = Button(root,
+                    text='open',
+                    width=10,
+                    height=2
+                    )
+btn_open.bind('<Button-1>',openFile)
+btn_open.grid(row=0,column=4)
+
+# 空一行
+Label(root).grid(row=1,column=0)
+
+def pcapAna(ptks):
+    
+    PD = protoAna.PcapDecode()
+    for p in ptks:
+        data = dict()
+        data = protoAna.PcapDecode.ether_decode(PD,p)
+        # 自定义协议内容解析
+        if p.haslayer("IP") & (p.version == 15):
+            payload = str(p.load, encoding = 'utf-8')
+            key = payload[:8]
+            sdata = payload[8:]
+            data["key"] = key
+            data["load"] = str(SE.des_decrypt(key,sdata), encoding='utf-8')
+        print(data)
+        for k,v in data.items():
+            dataCap.insert(END, str(k) + ": " + str(v) +"\n")
+        dataCap.insert(END,"\n")
+        dataCap.see(END)
+
+def NetCap(ip):
+    ptks = sniff(iface = IFACES.dev_from_index(12),count=5)
+    pcapAna(ptks)
+
+def dataRead(file):
+    ptks = rdpcap(file)
+    pcapAna(ptks)
 
 # 设置按钮控件
-
-# 捕获按钮回调函数
-def startCap(event):
-    ip = link.get()
-    if len(ip) == 0:
-        dataCap.insert(END, "请输入IP\n")
-        return
-    print("开始抓取数据, ip为：",ip)
-    # ip = "192.168.0.102"
-    
-    dics, packets = dataAna.sendAndCap(ip)
-    # 接收到数据不为空
-    if len(dics) != 0:
-        for dic in dics:
-            for k,v in dic.items():
-                print(str(k) + ":" + str(v))
-                dataAnalyse.insert(END,str(k)+":"+str(v)+'\n')
-    else:
-        dataAnalyse.insert(END,"received nothing!!!\n")
-    
-    dataCap.insert(END,packets)
-    
-    dataAnalyse.insert(END, "\n")
-    dataCap.insert(END, "\n")
-    dataCap.see(END)
-    dataAnalyse.see(END)
-
-Label(root).grid(row=1,column=0)
-btn_start = Button(root,
-                text='send&capture',
-                width=10,
-                height=2)
-btn_start.bind('<Button-1>',startCap)
-btn_start.grid(row=1, column=0,rowspan=2,columnspan=2)
-
-# 监听回调函数
-def sniffData(event):
-    pkts = sniff(iface=IFACES.dev_from_index(12),count=20) # 简单的抓取数据包
-    wrpcap("capted.pcap", pkts)  # 保存为demo.pcap
-        
-    PD = protoAna.PcapDecode()  # 实例化该类为PD
-    pcap_test = rdpcap("capted.pcap")  # 这个demo.pcap包含3次连接
-    data_result = dict()  # 将解析结果存入dict
-    for p in pcap_test:
-        data_result = protoAna.PcapDecode.ether_decode(PD, p)
-        print(data_result)
-        for k,v in data_result.items():
-            dataAnalyse.insert(END,str(k)+":"+str(v)+"\n")
-    dataCap.insert(END, pkts)
-
-    dataCap.insert(END,"\n")
-    dataAnalyse.insert(END,"\n")
-    dataCap.see(END)
-    dataAnalyse.see(END)
-
-btn_sniff = Button(root,
-                    text = 'sniff',
-                    height=2,
-                    state='active',
-                    width=10)
-btn_sniff.bind('<Button-1>', sniffData)
-btn_sniff.grid(row=1,column=2,rowspan=2,columnspan=2)
-
 # 数据解析回调函数
-def analyzeData(event):
-    # pcap_test = rdpcap("capted.pcap")
-    dataAnalyse.insert(END, "analysing data....\n")
+def startCap_Analyse(event):
     
-    PD = protoAna.PcapDecode()  # 实例化该类为PD
-    pcap_test = rdpcap("secret.pcap")  # 这个demo.pcap包含3次连接
-    data_result = dict()  # 将解析结果存入dict
-    for p in pcap_test:
-        data_result = protoAna.PcapDecode.ether_decode(PD, p)
-        for k,v in data_result.items():
-            dataAnalyse.insert(END,str(k)+": "+str(v)+"\n")
-        print(data_result)
-        if p.haslayer("Raw") == True:
-            secret_key = decodeKey.get()
-            if len(secret_key) != 8:
-                dataCap.insert(END, "please input exactly 8 bytes key to decode the data!!!\n"+
-                "originly maybe: madao233\n")
-            else:   
-                # 利用密钥解码load原始数据 
-                secret_data=SE.des_decrypt(secret_key, p.load)
-                dataAnalyse.insert(END, "Raw load: "+str(secret_data)+"\n")
-    dataCap.insert(END, pcap_test)
-    dataCap.insert(END,"\n")
-    dataAnalyse.insert(END,"\n")
-    dataCap.see(END)
-    dataAnalyse.see(END)
+    ip = link.get()
+    file = filePath.get()
+    chose = False
+    data = dict()
+    # 根据用户的选择，调用抓包函数或者本地文件
+    if len(ip) == 0 | len(file) == 0:
+        messagebox.showwarning("warnig!","There should be a ip or filepath at least!")#提出警告对话窗
+    elif len(ip) !=0 & len(file) != 0:
+        chose = messagebox.askquestion("conflict", "choose Ip?")
+        if(chose == True):
+            data = NetCap(ip)
+            return
+        else:
+            data = dataRead(file)
+            return
+    elif len(ip) !=0:
+        data = NetCap(ip)
+        return
+    else:
+        data = dataRead(file)
+        return    
 
-btn_analyze = Button(root,
-                        text='analyze data',
+# 开始按钮外观属性设置
+btn_start = Button(root,
+                        text='start',
                         height=2,
                         width=10)
-btn_analyze.bind('<Button-1>', analyzeData)
-btn_analyze.grid(row=1,column=4,rowspan=2,columnspan=2)
+btn_start.bind('<Button-1>', startCap_Analyse)
+btn_start.grid(row=2,column=0,rowspan=2,columnspan=2)
 
 # 消息文本控件
 dataCap = scrolledtext.ScrolledText(root,
@@ -148,21 +128,8 @@ dataCap = scrolledtext.ScrolledText(root,
                 bd='2',
                 bg='black',
                 fg='green',
-                width=75,
+                width=150,
                 height=30)
-# dataCap.insert(END, "hello, world!")
-dataCap.place(relx=0.25, rely=0.2, anchor=N)
-
-
-dataAnalyse = scrolledtext.ScrolledText(root,
-                    wrap=WORD,
-                    font=20,
-                    bd='2',
-                    bg='black',
-                    fg='green',
-                    width=75,
-                    height=30)
-dataAnalyse.place(relx=0.73, rely=0.2,anchor=N)
-
+dataCap.place(relx=0.5, rely=0.2, anchor=N)
 root.title('NET CAP TOOL')
 root.mainloop()
--- secretPcapMkTool.py
+++ secretPcapMkTool.py
@@ -1,8 +1,43 @@
 from scapy.all import *
-import structTest1 as ST
-secret_key = "madao233" # 必须是刚好8位字节的密码
-secret_data = "Live is like a box of chocolate, you never know what you gona get.-<FORREST GUMP>"
-secret_str = ST.des_encrypt(secret_key, secret_data)
-ptks = IP(dst = "www.baidu.com")/ICMP()/secret_str
-print(ptks.load)
-wrpcap("secret.pcap",ptks)
+import secretEncode as SE
+import struct
+import binascii
+import ctypes
+
+
+if __name__ == '__main__':
+    key = "12345678" # 必须是刚好8位字节的密码
+    src = "192.168.0.1"
+    data = "Live is like a box of chocolate, you never know what you gona get."
+    sdata = SE.des_encrypt(key, data)
+    print("加密后的数据：", sdata)
+    # IP/ICMP协议报文
+    raw_data = key + str(sdata, encoding='utf-8')
+    print("raw_data:", raw_data)
+    ptk1 = IP(dst="192.168.0.102",version=0xf)/ICMP()/Raw(raw_data)
+    print("IP version:", ptk1.version)
+    ptk2 = IP(dst="www.baidu.com")/data
+    ptk3 = IP()/ICMP()/"madao33"   
+
+    wrpcap('secretTest.pcap',ptk1)
+    wrpcap('unsecretTest.pcap', ptk2)
+    wrpcap('ipTest.pcap', ptk3)
+    ptks = rdpcap('secretTest.pcap')
+    print(ptks)
+    print(type(ptks[0]))
+    data = dict()
+    for ptk in ptks:
+        print("ip version:", ptk.version)
+        if (ptk.version == 15) & ptk.haslayer("IP"): # 未加密
+            print(ptk)
+            payload = str(ptk.load, encoding = 'utf-8')
+            print(payload)
+            key = payload[:8]
+            sdata = payload[8:]
+            print("key: ", key)
+            print("sdata: ", sdata)
+            udata = str(SE.des_decrypt(key,sdata), encoding='utf-8')
+            print("udata: ", udata)
+            data['raw'] = udata
+        
+    
